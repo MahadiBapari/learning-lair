60c835064db033f3463c724e77901c803b564199
Niloy <rafsnil@gmail.com>
2023-05-27 22:04:32 +0600

    Made some adjustments to the User reg form

--- src/Components/SignUpForm/SignUpFormStudent.js
+++ src/Components/SignUpForm/SignUpFormStudent.js
@@ -1,45 +1,49 @@
 import React from 'react'
-import styles from './SignUpFormStudent.module.css' 
+import styles from './SignUpFormStudent.module.css'
 import { useState } from 'react';
 import LogoDark from '../../images/logoDark.png'
 
 function SignUpFormStudent() {
-    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
+    const [usernumber, setUsernumber] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-  
+
     const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log('Submitted!', { email, username, password });
-      // ekhane backend chodaia further processing maraba
+        event.preventDefault();
+        console.log('Submitted!', { username, usernumber, email, password });
+
     };
-  
+
     return (
         <div className={styles.signUpFormStudent}>
-                <div className={styles.signUp_box}>
+            <div className={styles.signUp_box}>
 
-                        <img src={LogoDark} alt='Logo' />
-                        <form onSubmit={handleSubmit}>
-                            
-                        <div>
-                            <input placeholder='Email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <div>
-                            <input placeholder='Username' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                        </div>
-                        <div>
-                            <input placeholder='Password' type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        
-                            <button className={styles.btn} type="submit">Sign up</button>
-                        </form>
-                   
-                    
-                </div>
-                
+                <img src={LogoDark} alt='Logo' />
+                <form onSubmit={handleSubmit}>
+
+                    <div>
+                        <input placeholder='Username' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div>
+                        <input placeholder='User Number' type="text" value={usernumber} onChange={(e) => setUsernumber(e.target.value)} />
+                    </div>
+                    <div>
+                        <input placeholder='Email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div>
+                        <input placeholder='Password' type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+
+                    <button className={styles.btn} type="submit">Sign up</button>
+                </form>
+
+
+            </div>
+
         </div>
 
-    ); 
+    );
 }
 
 export default SignUpFormStudent
