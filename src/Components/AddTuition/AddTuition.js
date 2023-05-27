3f3a55e049c3462854e57d8e725902b4fae3204a
mahadiHasanBapari <hasan5481@gmail.com>
2023-05-27 19:38:10 +0600

    Add tuition modified

--- src/Components/AddTuition/AddTuition.js
+++ src/Components/AddTuition/AddTuition.js
@@ -1,26 +1,52 @@
 import React, { useState } from 'react';
-import PopupForm from './PopupForm.js';
+import PopupForm from './PopupForm';
+import axios from 'axios';
+import styles from './AddTuition.module.css'
 
-function AddTuition() {
+const AddTuition = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [posts, setPosts] = useState([]);
 
-    const [isFormVisible, setIsFormVisible] = useState(false);
+  const toggleFormVisibility = () => {
+    setIsFormVisible(!isFormVisible);
+  };
 
-    const toggleFormVisibility = () => {
-      setIsFormVisible(!isFormVisible);
-    };
+  const handleFormSubmit = (postData) => {
+    axios.post('/api/posts', postData)
+      .then(response => {
+        // Add the created post to the existing posts array
+        setPosts([...posts, response.data]);
+        // Close the form
+        toggleFormVisibility();
+      })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+      });
+  };
+
   return (
-    <div>
-        <h1>Welcome</h1>
-        <p>You can add tuition here</p>
-        <div>
-            <button onClick={toggleFormVisibility}>Add Tuition</button>
+    <div className={styles.addTuition}>
 
-            {isFormVisible && <PopupForm onClose={toggleFormVisibility} />}
+      <h1>Welcome</h1>
+      <p>You can add tuition here</p>
+      <button onClick={toggleFormVisibility}>Add Tuition</button>
+
+      {isFormVisible && <PopupForm onClose={toggleFormVisibility} onSubmit={handleFormSubmit} />}
+
+      {posts.map((post, index) => (
+        <div key={index} className="post">
+          <h2>{post.location}</h2>
+          <p>{post.phone}</p>
+          <p>{post.institution}</p>
+          <p>{post.type}</p>
+          <p>{post.subject}</p>
+          <p>{post.salary}</p>
+          <p>{post.extra}</p>
         </div>
+      ))}
     </div>
-    
+  );
+};
 
-  )
-}
-
-export default AddTuition
+export default AddTuition;
--- src/Components/AddTuition/PopupForm.js
+++ src/Components/AddTuition/PopupForm.js
@@ -1,54 +1,130 @@
-import React from 'react';
-import styles from './PopupForm.module.css';
+import React, { useState } from 'react';
 
-const PopupForm = ({ onClose }) => {
+const PopupForm = ({ onClose, onSubmit }) => {
+  
+  const [phone, setPhone] = useState('');
+  const [institution, setInstitution] = useState('');
+  const [type, setType] = useState('');
+  const [subject, setSubject] = useState('');
+  const [location, setLocation] = useState('');
+  const [salary, setSalary] = useState('');
+  const [extra, setExtra] = useState('');
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+  const handleInstitutionChange = (e) => {
+    setInstitution(e.target.value);
+  };
+  const handleTypeChange = (e) => {
+    setType(e.target.value);
+  };
+  const handleSubjectChange = (e) => {
+    setSubject(e.target.value);
+  };
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+  const handleSalaryChange = (e) => {
+    setSalary(e.target.value);
+  };
+  const handleExtraChange = (e) => {
+    setExtra(e.target.value);
+  };
+
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
+
+    const postData = {
+      phone: phone,
+      institution: institution,
+      type: type,
+      subject: subject,
+      location: location,
+      salary: salary,
+      extra: extra,
+        
+    };
+
+    onSubmit(postData);
 
     // Close the form after submission
     onClose();
   };
 
   return (
-    <div className={styles.popup}>
-      <div className={styles.popupContent}>
-        <h2>Tuition Form</h2>
+    <div className="popup">
+      <div className="popup-content">
+        <h2>Create a Post</h2>
         <form onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name" />
-          </div>
-
-          <div className={styles.formGroup}>
+          <div>
             <label htmlFor="phone">Phone Number:</label>
-            <input type="text" id="phone" name="phone" placeholder="Enter your phone number" />
+            <input
+              type="text"
+              id="phone"
+              value={phone}
+              onChange={handlePhoneChange}
+            />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="phone">School/Institution:</label>
-            <input type="text" id="school" name="phone" placeholder="Enter school/institution" />
+          <div>
+            <label htmlFor="institution">School/institution:</label>
+            <input
+              type="text"
+              id="institution"
+              value={institution}
+              onChange={handleInstitutionChange}
+            />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="phone">Class/type:</label>
-            <input type="text" id="grade" name="phone" placeholder="Enter Grade" />
+          <div>
+            <label htmlFor="type">Class/type:</label>
+            <input
+              type="text"
+              id="type"
+              value={type}
+              onChange={handleTypeChange}
+            />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="phone">Subjects:</label>
-            <input type="text" id="Subject" name="phone" placeholder="Enter subjects" />
+          <div>
+            <label htmlFor="subject">Subject:</label>
+            <input
+              type="text"
+              id="subject"
+              value={subject}
+              onChange={handleSubjectChange}
+            />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="phone">Location:</label>
-            <input type="text" id="location" name="phone" placeholder="Enter Location" />
+          <div>
+            <label htmlFor="location">location:</label>
+            <input
+              type="text"
+              id="location"
+              value={location}
+              onChange={handleLocationChange}
+            />
           </div>
-
-          <div className={styles.formGroup}>
-            <label htmlFor="message">Extra Innformation:</label>
-            <textarea id="message" name="message" placeholder="Add any information" />
+          <div>
+            <label htmlFor="salary">salary:</label>
+            <input
+              type="text"
+              id="salary"
+              value={salary}
+              onChange={handleSalaryChange}
+            />
           </div>
-
+          <div>
+            <label htmlFor="extra">Extra Info:</label>
+            <input
+              type="text"
+              id="extra"
+              value={extra}
+              onChange={handleExtraChange}
+            />
+          </div>
+          
           <button type="submit">Submit</button>
         </form>
-        <button className={styles.closeButton} onClick={onClose}>Close</button>
+        <button className="close-button" onClick={onClose}>Close</button>
       </div>
     </div>
   );
