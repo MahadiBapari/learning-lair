1102236c8c685386e6cf1f492992b75033f03d44
mahadiHasanBapari <hasan5481@gmail.com>
2023-05-27 22:02:42 +0600

    login redirect added

--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -1,66 +1,61 @@
-import styles from './SignInForm.module.css'
 import React, { useState } from 'react';
-import LogoDark from '../../images/logoDark.png'
-import { Link } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom';
+import LogoDark from '../../images/logoDark.png';
+import styles from './SignInForm.module.css';
+import axios from 'axios';
 
-function SignInForm( props ) {
+const SignInForm = (props) => {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
+
+    // Perform login API call
+    axios.post('/api/login', { email, password })
+      .then(response => {
+        
+        history.push('/tuition');
+      })
+      .catch(error => {
+        // Handle login error
+        setError('Invalid email or password');
+        console.error(error);
+      });
   };
 
   return (
-
     <div className={styles.signInForm}>
-        <div className={styles.signIn_box}>
-
-                <img src={LogoDark} alt='Logo' />
-                <h2>{props.name}</h2>
-            
-            <form onSubmit={handleSubmit}>
-                
-                <div>
-                    <input
-                        placeholder='Email'
-                        type="email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                        />
-                </div>            
-                           
-                <div>
-                    <input
-                        placeholder='Password'
-                        type="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                        />
-                </div>  
-                    
-                <Link to="/tuition">
-                <button className={styles.btn} type="submit">Sign In</button>
-                </Link>
-                
-                
-                
-                </form>
-                
-                <p>Dont have an account?</p>
-                <Link to="/signupstudent">
-                  <button className={styles.btn2}>Register Now</button>
-                </Link>
-                
-
-
-        </div>
-        
+      <div className={styles.signIn_box}>
+        <img src={LogoDark} alt='Logo' />
+        <h2>{props.name}</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              placeholder='Email'
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              placeholder='Password'
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          {error && <p>{error}</p>}
+          <button className={styles.btn} type="submit">Sign In</button>
+        </form>
+        <p>Don't have an account?</p>
+        <Link to="/signupstudent" className={styles.btn2}>Register Now</Link>
+      </div>
     </div>
-    
   );
-}
+};
 
 export default SignInForm;
